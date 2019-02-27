72cf4e38058f25e77c91c9c21e67909b68dccdb0
jclbrooks <jbrooks0@frostburg.edu>
2019-02-26 23:23:35 -0500

    added code to format capitals data, shenandoah data, latter doesn't work

--- Landscape_MSOM.R
+++ Landscape_MSOM.R
@@ -5,24 +5,47 @@
 library(dplyr)
 library(tidyr)
 
+# Read in data
 canaan <- read.csv("Data/Landscape/CVNWR_transects.csv", header = T, stringsAsFactors = F)
-capital <- read.csv("Data/Landscape/NCRlotic_all.csv", header = T)
+capital <- read.csv("Data/Landscape/NCRlotic_all.csv", header = T, stringsAsFactors = F)
 shenandoah <- read.csv("Data/Landscape/Shen_snp12.csv", header = T)
 
 str(canaan)
 str(capital)
+str(shenandoah)
 
+# Format data: transect ID - species - age - pass/visit 1- pass/visit 2 - pass/visit - 3
+# made all same format, column names
 can <- canaan %>%
   mutate(Transect = paste(Name, Transect, Year, sep = "_")) %>%
   group_by(Transect, Species, Age) %>%
   select(Transect, Pass, Species, Age, Caught) %>%
   mutate(Pass = paste0("p", Pass)) %>%
   spread(Pass, Caught)
+colnames(can) <- c("transect", "species", "age", "pass1", "pass2", "pass3", "pass4")
 
 cap <- capital %>%
-  mutate(Transect = paste(PointName, Transect, Year, sep = "_")) %>%
-  group_by(Transect, Species, Age) %>%
-  select(Transect, Pass, Species, Age, Caught) %>%
-  mutate(Pass = paste0("p", Pass)) %>%
-  spread(Pass, Caught)
-  
+  mutate(Transect = paste(PointName, Visit, sep = "_v"),
+         pass4 = "NULL") %>% # added pass4 column to match canaan dataframe
+  group_by(Transect, SpeciesCode, SAgeID) %>%
+  select(Transect, SpeciesCode, SAgeID, PassCount1, PassCount2, PassCount3, pass4)
+colnames(cap) <- c("transect", "species", "age", "pass1", "pass2", "pass3", "pass4")
+
+# list <- c(shenandoah$Site, shenandoah$Species, shenandoah$Age)
+# add_count(shenandoah, name = "count")
+
+she <- shenandoah  %>%
+  group_by(Site, Species, Age) %>%
+  select(Site, Species, Age, Pass, count) %>%
+  mutate(Pass = paste0("pass", Pass)) %>%
+  spread(Pass, count)
+# shenandoah[452,]
+# shenandoah[453,]
+# shenandoah[467,]
+# shenandoah$X.
+
+# Remove NULLs from capitals data
+na <- cap[which(cap$species == "NULL"),]
+cap1 <- cap[-which(cap$species == "NULL"),]
+cap <- cap1
+ 
