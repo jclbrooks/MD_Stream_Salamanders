3a73fa4ac47aec22bdb318d0381e5da172d41fe7
jclbrooks <jbrooks0@frostburg.edu>
2019-03-06 10:39:30 -0500

    added 4th model for pH and time_min

--- Code/prep_data.R
+++ Code/prep_data.R
@@ -464,6 +464,100 @@ cat("
 sink()
 
 
+########################################################
+# MSOM - beta1: pH , alpha1: time_min - add covariance with occupancy and detection
+sink("Code/JAGS/multi_spp_occ4.txt")
+cat("
+    model {
+    
+    #PRIORS
+    
+    # Set species loop
+    for(i in 1:n_species) {
+    
+    b0[i] ~ dnorm(mu.b0, tau.b0) 
+    b1[i] ~ dnorm(mu.b1, tau.b1)
+
+    a0.exp[i] <- mu.a0 + (rho * sigma.p / sigma.psi) * (b0[i] - mu.b0)    
+    a0[i] ~ dnorm(a0.exp[i], var.p) # expected mean detection for each species over mean covariate conditions
+    a1[i] ~ dnorm(mu.a1, tau.a1)
+    }
+    
+    #HYPERPRIORS
+    mu.b0 ~ dnorm(0, 0.01)
+    # sigma.psi ~ dt(0, pow(3, -2), 1)T(0, ) # half cauchy with sd = 3
+    sigma.psi ~ dunif(0, 5)
+    tau.b0 <- 1 / sigma.psi / sigma.psi
+    # tau.b0 ~ dgamma(0.1, 0.1)
+    # psi.mean ~ dunif(0, 1) # real prob scale mean occupancy among species
+    # mu.b0 <- log(psi.mean / (1 - psi.mean)) # mean occupancy among species on logit scale
+    
+    mu.b1 ~ dnorm(0, 0.01)
+    tau.b1 ~ dgamma(0.1, 0.1)
+    
+    mu.a0 ~ dnorm(0, 0.01)
+    sigma.p ~ dunif(0, 10)
+    # sigma.p ~ dt(0, pow(5, -2), 1)T(0, ) # half cauchy with sd = 1 # informative prior pulling things close to the center
+    tau.a0 <- 1 / sigma.p / sigma.p
+    # tau.a0 ~ dgamma(0.1, 0.1)
+    var.p <- tau.b0 / (1.-pow(rho, 2))
+
+    rho ~ dunif(-1, 1) # prior correlation between occupancy and detection
+    
+    mu.a1 ~ dnorm(0, 0.01)
+    tau.a1 ~ dgamma(0.1, 0.1)
+    
+    # array transect (j) x visit (k) x species (i) x stage (l)
+    # Estimation of Z matrix (true occurrence for species i at site j)	
+    for(i in 1:n_species) {	
+      for(j in 1:n_trans) { 
+        for(l in 1:n_stages) {
+          logit(psi[j,i,l]) <- b0[i] + b1[i]*pH[j]
+    
+          Z[j,i,l] ~ dbern(psi[j,i,l])
+    
+          # Estimate detection for species i and stage k at transect j during visit k
+          for (k in 1:n_visits) {
+            logit(p[j,k,i,l]) <- a0[i] + a1[i] * time_min[j, k]
+            mu.p[j,k,i,l] <- p[j,k,i,l] * Z[j,i,l]
+            y[j,k,i,l] ~ dbern(mu.p[j,k,i,l])
+    
+          }
+        }
+      }
+    }
+    
+    ###### Derived quantities #######
+    
+    
+    for(i in 1:n_species) {
+    for(j in 1:n_trans) {
+    occ_spp_stage[j, i] <- max(Z[ j, i, ])
+    }
+    occ_sp[i] <- sum(occ_spp_stage[ , i])        # Number of occupied transects by this species
+    }
+    
+    for(j in 1:n_trans) {
+    for(i in 1:n_species) {
+    occ_trans_sp[j, i] <- max(Z[j, i, ])
+    }
+    occ_trans[j] <- sum(occ_trans_sp[j, ])      # Number of occurring species at each transect
+    }
+    
+    # mean detection by species and stage
+    for(i in 1:n_species) {
+    for(l in 1:n_stages) {
+    p_sp[i, l] <- sum(p[ , , i, l])
+    }
+    }
+    
+    # overall mean detection
+    p_mean <- mean(p)
+    
+    
+    }
+    ", fill=TRUE)
+sink()
 
 
 
@@ -509,21 +603,60 @@ if(testing) {
 
 library(jagsUI)
 
-out1 <- jags(data = data_list, inits = inits, params, model.file = "Code/JAGS/multi_spp_occ3.txt", 
+# beta1: pH, alpha1: time_min
+out1 <- jags(data = data_list, inits = inits, params, model.file = "Code/JAGS/multi_spp_occ2.txt", 
+            n.chains = nc,
+            n.burnin = nb,
+            n.iter = ni,
+            parallel = TRUE,
+            bugs.format = TRUE)
+# beta1: pH, alpha1: time_min w/ added covariance of occupancy with detection
+out4 <- jags(data = data_list, inits = inits, params, model.file = "Code/JAGS/multi_spp_occ2.txt", 
+            n.chains = nc,
+            n.burnin = nb,
+            n.iter = ni,
+            parallel = TRUE,
+            bugs.format = TRUE)
+# beta1: ec, alpha1: water
+out2 <- jags(data = data_list, inits = inits, params, model.file = "Code/JAGS/multi_spp_occ2.txt", 
+            n.chains = nc,
+            n.burnin = nb,
+            n.iter = ni,
+            parallel = TRUE,
+            bugs.format = TRUE)
+# beta1: ec, alpha1: water w/ added covariance of occupancy with detection
+out3 <- jags(data = data_list, inits = inits, params, model.file = "Code/JAGS/multi_spp_occ3.txt", 
             n.chains = nc,
             n.burnin = nb,
             n.iter = ni,
             parallel = TRUE,
             bugs.format = TRUE)
 
-out <- out1
+
+#out <- out1
 
 summary(out, parameters = c("mu.b0", "tau.b0", "mu.b1", "tau.b1", "mu.a0", "tau.a0", "mu.a1", "tau.a1"))
 
-traceplot(out, parameters = c("rho", "mu.b0", "mu.a0", "sigma.psi", "sigma.p", "a0", "b0"))
+# ###traceplot(out4, parameters = c("rho",
+#                               "mu.b0", 
+#                               "mu.a0", 
+#                               "sigma.psi", 
+#                               "sigma.p", 
+#                               "a0"
+#                               ,"b0"
+#                               ))
 
-traceplot(out, parameters = c("mu.b0", "tau.b0", "mu.a0", "tau.a0"))
-whiskerplot(out, parameters = c("rho", "mu.b0", "sigma.psi", "mu.b1", "tau.b1", "mu.a0", "sigma.p", "mu.a1", "tau.a1"))
+traceplot(out1, parameters = c("mu.b0", "tau.b0", "mu.a0", "tau.a0", "a0", "b0"))
+traceplot(out4, parameters = c("mu.b0", "tau.b0", "mu.a0", "tau.a0", "a0", "b0"))
+
+################
+# out1 vs out4 - rho (out4) seems less informative but doesn't mess things up too much
+# out2 vs out3 - rho (out3) is non-informative and doesn't allow models to converge
+
+
+out <- out4
+
+###whiskerplot(out, parameters = c("rho", "mu.b0", "sigma.psi", "mu.b1", "tau.b1", "mu.a0", "sigma.p", "mu.a1", "tau.a1"))
 whiskerplot(out, parameters = c("p_mean"))
 whiskerplot(out, parameters = c("b1")) # effects of pH by species
 whiskerplot(out, parameters = c("a1")) # effects of pH by species
