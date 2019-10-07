d7fb508044f3ec43abfc390447e05e0bfc356e54
jclbrooks <jbrooks0@frostburg.edu>
2019-10-07 00:23:28 -0400

    added mean_wetted_width and mean_depth covariates to GLMMs

--- Code/run_glm.R
+++ Code/run_glm.R
@@ -51,15 +51,36 @@ spec <- sal %>%
 sallies <- sal %>%
   left_join(spec) %>%
   select(-DFUSA, -DFUSL, -DOCHL, -DOCHA, -DMONL, -DMONA, -EBISL, -EBISA, -GPORL, -GPORA, -PRUBL, -PRUBA) %>%
-  mutate(transect = paste0(stream, "_", transect),
-         transect_num = as.integer(as.factor(transect)))
+  mutate(stream_trans = paste0(stream, "_", transect),
+         transect_num = as.integer(as.factor(transect)),
+         date = mdy(date))
+
+width <- read.csv("Data/Date_Location_Transect_Visit_Data_Physical_Stream_Features_processed.csv", stringsAsFactors = FALSE, header = T)
+
+width1 <- width %>%
+  select(-Section, -X) %>%
+  group_by(Date, Stream, Transect, Visit) %>%
+  summarise(mean(Wetted),
+            mean(Depth))
+  
+width2 <- width1[-1,] %>%
+  ungroup()# %>%
+ # mutate(Date = mdy(Date))
+
+str(width2)
+colnames(width2) <- c("date", "stream", "transect", "visit", "mean_wetted_width", "mean_depth")
+as.data.frame(width2)
+
+sal_all <- sallies %>%
+  left_join(width2, by = c("stream", "transect", "visit")) %>%
+  select(-date.y, -DO)
 
 
 # Checking for correlation
 library(corrplot)
 library(car)
-cov <- sallies %>%
-  select(air, pH, water, EC, TDS) 
+cov <- sal_all %>%
+  select(air, pH, water, EC, TDS, mean_wetted_width, mean_depth) 
 cov <- na.omit(cov)
 corrplot(corr = cov)
 cor(cov)   # can also use vif() variance inflation factor, toss out anything above 10, ideal <3
@@ -73,7 +94,7 @@ vif(dfus1)
 par(mfrow=c(1,3))
 
 # DFUS
-dfus1 <- glmer(DFUS ~ air + EC + (1 | transect_num), family = poisson, data = sallies)
+dfus1 <- glmer(DFUS ~ air + pH + EC + mean_wetted_width + mean_depth + (1 | transect_num), family = poisson, data = sal_all)
 summary(dfus1)
 plot(dfus1)
 qqnorm(residuals(dfus1))
@@ -82,21 +103,7 @@ hist(residuals(dfus1))
 # removed pH from model because it wouldn't converge with it in it
 # none of the covariates are statistically significant
 
-dfus2 <- glmer(DFUS ~ air + (1 | transect_num), family = poisson, data = sallies)
-summary(dfus2)
-plot(dfus2)
-qqnorm(residuals(dfus2))
-boxplot(residuals(dfus2))
-hist(residuals(dfus2))
-
-dfus3 <- glmer(DFUS ~ pH + (1 | transect_num), family = poisson, data = sallies)
-summary(dfus3)
-plot(dfus3)
-qqnorm(residuals(dfus3))
-boxplot(residuals(dfus3))
-hist(residuals(dfus3))
-
-dfus4 <- glmer(DFUS ~ EC + (1 | transect_num), family = poisson, data = sallies)
+dfus4 <- glmer(DFUS ~ water + (1 | transect_num), family = poisson, data = sal_all)
 summary(dfus4)
 plot(dfus4)
 qqnorm(residuals(dfus4))
@@ -104,7 +111,7 @@ boxplot(residuals(dfus4))
 hist(residuals(dfus4))
   
 # DMON
-dmon1 <- glmer(DMON ~ air + pH + EC + (1 | transect_num), family = poisson, data = sallies)
+dmon1 <- glmer(DMON ~ air + pH + EC + (1 | transect_num), family = poisson, data = sal_all)
 summary(dmon1)
 plot(dmon1)
 qqnorm(residuals(dmon1))
@@ -113,8 +120,27 @@ hist(residuals(dmon1))
 ### air is statistically significant, the residuals look good!
 ###--- PLOT ---###
 
+dmon2 <- glmer(DMON ~ air + pH + EC + mean_wetted_width + mean_depth + (1 | transect_num), family = poisson, data = sal_all)
+summary(dmon2)
+plot(dmon2)
+qqnorm(residuals(dmon2))
+boxplot(residuals(dmon2))
+hist(residuals(dmon2))
+### depth is statistically significant, the residuals look okay
+###--- PLOT ---###
+
+dmon3 <- glmer(DMON ~ water + (1 | transect_num), family = poisson, data = sal_all)
+summary(dmon3)
+plot(dmon3)
+qqnorm(residuals(dmon3))
+boxplot(residuals(dmon3))
+hist(residuals(dmon3))
+### water is significant, residuals look okay
+
+
+
 # DOCH
-doch1 <- glmer(DOCH ~ air + pH + EC + (1 | transect_num), family = poisson, data = sallies)
+doch1 <- glmer(DOCH ~ air + pH + EC + mean_wetted_width + mean_depth + (1 | transect_num), family = poisson, data = sal_all)
 summary(doch1)
 plot(doch1)
 qqnorm(residuals(doch1))
@@ -122,60 +148,54 @@ boxplot(residuals(doch1))
 hist(residuals(doch1))
 # only use air because pH and EC cause the model's parameters to be on the boudnary of the feasible parameter space, look up ?isSingular for explanation
 
-doch2 <- glmer(DOCH ~ air + (1 | transect_num), family = poisson, data = sallies)
-summary(doch2)
-plot(doch2)
-qqnorm(residuals(doch2))
-boxplot(residuals(doch2))
-hist(residuals(doch2))
-# runs fine but is not significant
-
-doch3 <- glmer(DOCH ~ water + (1 | transect_num), family = poisson, data = sallies)
+doch3 <- glmer(DOCH ~ water + (1 | transect_num), family = poisson, data = sal_all)
 summary(doch3)
 plot(doch3)
 qqnorm(residuals(doch3))
 boxplot(residuals(doch3))
 hist(residuals(doch3))
-# push parameters to boundary but is significant, residuals are not great
+# wwater is significant, residuals are not great
 ###--- PLOT ---###
 
-doch4 <- glmer(DOCH ~ pH + (1 | transect_num), family = poisson, data = sallies)
-summary(doch4)
-plot(doch4)
-qqnorm(residuals(doch4))
-boxplot(residuals(doch4))
-hist(residuals(doch4))
-# push parameters to boundary, is not significant
 
-doch5 <- glmer(DOCH ~ EC + (1 | transect_num), family = poisson, data = sallies)
-summary(doch5)
-plot(doch5)
-qqnorm(residuals(doch5))
-boxplot(residuals(doch5))
-hist(residuals(doch5))
-# push parameters to boundary, is not significant
 
 # EBIS
-ebis1 <- glmer(EBIS ~ air + pH + EC + (1 | transect_num), family = poisson, data = sallies)
+ebis1 <- glmer(EBIS ~ air + pH + EC + mean_wetted_width + mean_depth + (1 | transect_num), family = poisson, data = sal_all)
 summary(ebis1)
 plot(ebis1)
 qqnorm(residuals(ebis1))
 boxplot(residuals(ebis1))
 hist(residuals(ebis1))
-# all significant, some clustering in residuals but not terrible
+# air, pH, EC significant, some clustering in residuals but not terrible
 ###--- PLOT ---###
 
+ebis2 <- glmer(EBIS ~ water + (1 | transect_num), family = poisson, data = sal_all)
+summary(ebis2)
+plot(ebis2)
+qqnorm(residuals(ebis2))
+boxplot(residuals(ebis2))
+hist(residuals(ebis2))
+### water is significant, residuals look good
+
 # GPOR
-gpor1 <- glmer(GPOR ~ air + pH + EC + (1 | transect_num), family = poisson, data = sallies)
+gpor1 <- glmer(GPOR ~ air + pH + EC + mean_wetted_width + mean_depth + (1 | transect_num), family = poisson, data = sal_all)
 summary(gpor1)
 plot(gpor1)
 qqnorm(residuals(gpor1))
 boxplot(residuals(gpor1))
 hist(residuals(gpor1))
-# only intercept is significant
+# mean_wetted_width, mean_depth is significant, residuals okay
+
+gpor2 <- glmer(GPOR ~ water + (1 | transect_num), family = poisson, data = sal_all)
+summary(gpor2)
+plot(gpor2)
+qqnorm(residuals(gpor2))
+boxplot(residuals(gpor2))
+hist(residuals(gpor2))
+# water not significa
 
 # PRUB
-prub1 <- glmer(PRUB ~ air + pH + EC + (1 | transect_num), family = poisson, data = sallies)
+prub1 <- glmer(PRUB ~ air + pH + EC + mean_wetted_width + mean_depth + (1 | transect_num), family = poisson, data = sal_all)
 summary(prub1)
 plot(prub1)
 qqnorm(residuals(prub1))
