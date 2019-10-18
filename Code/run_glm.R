33cf2b398826c433e1a3a34f0ea2e8aa1971fe10
jclbrooks <jbrooks0@frostburg.edu>
2019-10-18 00:29:42 -0400

    added cobble to all models and code to look at water temp vs water chem vs physical features

--- Code/run_glm.R
+++ Code/run_glm.R
@@ -12,6 +12,7 @@ library(lubridate)
 library(AICcmodavg)
 
 sal <- read.csv("Data/Date_Location_Transect_Visit_Data_Processed.csv", stringsAsFactors = FALSE)
+substrate <- read.csv("C:/Users/Jacey/Documents/FSU/Research/R_code/MD_Stream_salamanders/Data/Location_Transect_Stream_Features_processed.csv", stringsAsFactors = FALSE)
 
 # ---- Separate adults and larvae analyses ----
 
@@ -73,8 +74,30 @@ str(width2)
 colnames(width2) <- c("date", "stream", "transect", "visit", "mean_wetted_width", "mean_depth")
 as.data.frame(width2)
 
+sub <- substrate %>%
+  select(-Date, - Visit, - Section, -X) %>%
+  group_by(Stream, Transect) %>%
+  summarise_all(max, na.rm = TRUE) %>%
+  mutate(Bankfull = ifelse(Bankfull == (-Inf), NA, Bankfull),
+         S.C = ifelse(S.C == (-Inf), NA, S.C),
+         Sand = ifelse(Sand == (-Inf), NA, Sand),
+         Gravel = ifelse(Gravel == (-Inf), NA, Gravel),
+         Cobble = ifelse(Cobble == (-Inf), NA, Cobble),
+         Boulder = ifelse(Boulder == (-Inf), NA, Boulder),
+         Debris = ifelse(Debris == (-Inf), NA, Debris),
+         perc_s.c = (S.C/25)*100,
+         perc_sand = (Sand/25)*100,
+         perc_gravel = (Gravel/25)*100,
+         perc_cobble = (Cobble/25)*100,
+         perc_boulder = (Boulder/25)*100,
+         perc_debris = (Debris/25)*100) %>%
+  select(-S.C, -Sand, -Gravel, -Cobble, -Boulder, -Debris)
+colnames(sub) <- c("stream", "transect", "bankfull", "silt_clay", "sand", "gravel", "cobble", "boulder", "debris")
+
+
 sal_all <- sallies %>%
   left_join(width2, by = c("stream", "transect", "visit")) %>%
+  left_join(sub) %>%
   select(-date.y, -DO)
 
 
@@ -82,30 +105,31 @@ sal_all <- sallies %>%
 library(corrplot)
 library(car)
 cov <- sal_all %>%
-  select(air, pH, water, EC, TDS, mean_wetted_width, mean_depth) 
+  select(air, pH, water, EC, TDS, mean_wetted_width, mean_depth, cobble) 
 cov <- na.omit(cov)
 corrplot(corr = cov)
-cor(cov)   # can also use vif() variance inflation factor, toss out anything above 10, ideal <3
+cor(cov, use = "complete.obs")   # can also use vif() variance inflation factor, toss out anything above 10, ideal <3
 plot(cor(cov))
 ## EC and TDS are highly correlated, EC and TDS are somewhat correlated with water temp
 vif(dfus1)
 
 
 # ---- Exploratory GLMMs ----
+# water temp vs water chemistry
 
 par(mfrow=c(1,3))
 
 # DFUS
 dfus = list()
 
-dfus[[1]] <- glmer(DFUS ~ water + mean_wetted_width + (1 | transect_num), family = poisson, data = sal_all)
+dfus[[1]] <- glmer(DFUS ~ water + mean_wetted_width + cobble + (1 | transect_num), family = poisson, data = sal_all)
 
-sal_all$pH <- scale(sal_all$pH)
+# sal_all$pH <- scale(sal_all$pH)
 #sal_all$EC <- scale(sal_all$EC)
 
-dfus[[2]] <- glmer(DFUS ~ pH + EC + mean_wetted_width + (1 | transect_num), family = poisson, data = sal_all)
+dfus[[2]] <- glmer(DFUS ~ pH + EC + mean_wetted_width + cobble + (1 | transect_num), family = poisson, data = sal_all)
 
-model_names = c("water + mean_wetted_width", "pH + EC + mean_wetted_width")
+model_names = c("water + mean_wetted_width + cobble", "pH + EC + mean_wetted_width + cobble")
 aictab(cand.set = dfus, modnames = model_names)
 
 summary(dfus[[2]])
@@ -119,11 +143,11 @@ hist(residuals(dfus[[2]]))
 # DMON
 dmon = list()
 
-dmon[[1]] <- glmer(DMON ~ water + mean_wetted_width + (1 | transect_num), family = poisson, data = sal_all)
+dmon[[1]] <- glmer(DMON ~ water + mean_wetted_width + cobble + (1 | transect_num), family = poisson, data = sal_all)
 
-dmon[[2]] <- glmer(DMON ~  pH + EC + mean_wetted_width + (1 | transect_num), family = poisson, data = sal_all)
+dmon[[2]] <- glmer(DMON ~  pH + EC + mean_wetted_width + cobble + (1 | transect_num), family = poisson, data = sal_all)
 
-model_names = c("water + mean_wetted_width", "pH + EC + mean_wetted_width")
+model_names = c("water + mean_wetted_width + cobble", "pH + EC + mean_wetted_width + cobble")
 aictab(cand.set = dmon, modnames = model_names)
 
 summary(dmon[[2]])
@@ -131,17 +155,19 @@ plot(dmon[[2]])
 qqnorm(residuals(dmon[[2]]))
 boxplot(residuals(dmon[[2]]))
 hist(residuals(dmon[[2]]))
+
+saveRDS(dmon[[2]], "Results/dmon_glmm_results.rds")
 ### pH is statistically significant, the residuals look okay
 ###--- PLOT ---###
 
 # DOCH
 doch = list()
 
-doch[[1]] <- glmer(DOCH ~ water + mean_wetted_width  + (1 | transect_num), family = poisson, data = sal_all)
+doch[[1]] <- glmer(DOCH ~ water + mean_wetted_width + cobble  + (1 | transect_num), family = poisson, data = sal_all)
 
-doch[[2]] <- glmer(DOCH ~ pH + EC + mean_wetted_width + (1 | transect_num), family = poisson, data = sal_all)
+doch[[2]] <- glmer(DOCH ~ pH + EC + mean_wetted_width + cobble + (1 | transect_num), family = poisson, data = sal_all)
 
-model_names = c("water + mean_wetted_width", "pH + EC + mean_wetted_width")
+model_names = c("water + mean_wetted_width + cobble", "pH + EC + mean_wetted_width + cobble")
 aictab(cand.set = doch, modnames = model_names)
 
 ### both models failed to converge
@@ -150,11 +176,11 @@ aictab(cand.set = doch, modnames = model_names)
 # EBIS
 ebis = list()
 
-ebis[[1]] <- glmer(EBIS ~ water + mean_wetted_width + (1 | transect_num), family = poisson, data = sal_all)
+ebis[[1]] <- glmer(EBIS ~ water + mean_wetted_width + cobble + (1 | transect_num), family = poisson, data = sal_all)
 
-ebis[[2]] <- glmer(EBIS ~ pH + EC + mean_wetted_width + (1 | transect_num), family = poisson, data = sal_all)
+ebis[[2]] <- glmer(EBIS ~ pH + EC + mean_wetted_width + cobble + (1 | transect_num), family = poisson, data = sal_all)
 
-model_names = c("water + mean_wetted_width", "pH + EC + mean_wetted_width")
+model_names = c("water + mean_wetted_width + cobble", "pH + EC + mean_wetted_width + cobble")
 aictab(cand.set = ebis, modnames = model_names)
 
 summary(ebis[[2]])
@@ -167,15 +193,15 @@ hist(residuals(ebis[[2]]))
 # GPOR
 gpor = list()
 
-gpor[[1]] <- glmer(GPOR ~ water + mean_wetted_width + (1 | transect_num), family = poisson, data = sal_all)
+gpor[[1]] <- glmer(GPOR ~ water + mean_wetted_width + cobble + (1 | transect_num), family = poisson, data = sal_all)
 
-gpor[[2]] <- glmer(GPOR ~ pH + EC + mean_wetted_width + (1 | transect_num), family = poisson, data = sal_all)
+gpor[[2]] <- glmer(GPOR ~ pH + EC + mean_wetted_width + cobble + (1 | transect_num), family = poisson, data = sal_all)
 
-model_names = c("water + mean_wetted_width", "pH + EC + mean_wetted_width")
+model_names = c("water + mean_wetted_width + cobble", "pH + EC + mean_wetted_width + cobble")
 aictab(cand.set = gpor, modnames = model_names)
 
-summary(gpor[[2]])
-plot(gpor[[2]])
+summary(gpor[[1]])
+plot(gpor[[1]])
 qqnorm(residuals(gpor[[2]]))
 boxplot(residuals(gpor[[2]]))
 hist(residuals(gpor[[2]]))
@@ -184,14 +210,129 @@ hist(residuals(gpor[[2]]))
 # PRUB
 prub = list()
 
-prub[[1]] <- glmer(PRUB ~ water + mean_wetted_width + (1 | transect_num), family = poisson, data = sal_all)
-prub[[2]] <- glmer(PRUB ~ pH + EC + mean_wetted_width + (1 | transect_num), family = poisson, data = sal_all)
+prub[[1]] <- glmer(PRUB ~ water + mean_wetted_width + cobble + (1 | transect_num), family = poisson, data = sal_all)
+prub[[2]] <- glmer(PRUB ~ pH + EC + mean_wetted_width + cobble + (1 | transect_num), family = poisson, data = sal_all)
 
-model_names = c("water + mean_wetted_width", "pH + EC + mean_wetted_width")
+model_names = c("water + mean_wetted_width + cobble", "pH + EC + mean_wetted_width + cobble")
 aictab(cand.set = prub, modnames = model_names)
 
 # response is constant since there was just one count
 
 
+##################################################################
+# Exploring water temp vs water chemistry vs physical features
+
+
+# DFUS
+dfus = list()
+
+dfus[[1]] <- glmer(DFUS ~ water + (1 | transect_num), family = poisson, data = sal_all)
+
+# sal_all$pH <- scale(sal_all$pH)
+#sal_all$EC <- scale(sal_all$EC)
+
+dfus[[2]] <- glmer(DFUS ~ pH + EC +  (1 | transect_num), family = poisson, data = sal_all)
+
+dfus[[3]] <- glmer(DFUS ~ mean_wetted_width + cobble + (1 | transect_num), family = poisson, data = sal_all)
+
+model_names = c("water", "pH + EC", "mean_wetted_width + cobble")
+aictab(cand.set = dfus, modnames = model_names)
+
+summary(dfus[[3]])
+plot(dfus[[2]])
+qqnorm(residuals(dfus[[2]]))
+boxplot(residuals(dfus[[2]]))
+hist(residuals(dfus[[2]]))
+# none of the variables are significant, residuals aren't great
+
+  
+# DMON
+dmon = list()
+
+dmon[[1]] <- glmer(DMON ~ water + (1 | transect_num), family = poisson, data = sal_all)
+
+dmon[[2]] <- glmer(DMON ~ pH + EC +  (1 | transect_num), family = poisson, data = sal_all)
+
+dmon[[3]] <- glmer(DMON ~ mean_wetted_width + cobble + (1 | transect_num), family = poisson, data = sal_all)
+
+model_names = c("water", "pH + EC", "mean_wetted_width + cobble")
+aictab(cand.set = dmon, modnames = model_names)
+
+summary(dmon[[3]])
+plot(dmon[[3]])
+qqnorm(residuals(dmon[[2]]))
+boxplot(residuals(dmon[[2]]))
+hist(residuals(dmon[[2]]))
+
+saveRDS(dmon[[2]], "Results/dmon_glmm_results.rds")
+### pH is statistically significant, the residuals look okay
+###--- PLOT ---###
+
+# DOCH
+doch = list()
+
+doch[[1]] <- glmer(DOCH ~ water + (1 | transect_num), family = poisson, data = sal_all)
+
+doch[[2]] <- glmer(DOCH ~ pH + EC +  (1 | transect_num), family = poisson, data = sal_all)
+
+doch[[3]] <- glmer(DOCH ~ mean_wetted_width + cobble + (1 | transect_num), family = poisson, data = sal_all)
+
+model_names = c("water", "pH + EC", "mean_wetted_width + cobble")
+aictab(cand.set = doch, modnames = model_names)
+
+### both models failed to converge
+
+
+# EBIS
+ebis = list()
+
+ebis[[1]] <- glmer(EBIS ~ water + (1 | transect_num), family = poisson, data = sal_all)
+
+ebis[[2]] <- glmer(EBIS ~ pH + EC +  (1 | transect_num), family = poisson, data = sal_all)
+
+ebis[[3]] <- glmer(EBIS ~ mean_wetted_width + cobble + (1 | transect_num), family = poisson, data = sal_all)
+
+model_names = c("water", "pH + EC", "mean_wetted_width + cobble")
+aictab(cand.set = ebis, modnames = model_names)
+
+summary(ebis[[2]])
+plot(ebis[[2]])
+qqnorm(residuals(ebis[[2]]))
+boxplot(residuals(ebis[[2]]))
+hist(residuals(ebis[[2]]))
+### nothing is significant, residuals look GREAT
+
+# GPOR
+gpor = list()
+
+gpor[[1]] <- glmer(GPOR ~ water + (1 | transect_num), family = poisson, data = sal_all)
+
+gpor[[2]] <- glmer(GPOR ~ pH + EC +  (1 | transect_num), family = poisson, data = sal_all)
+
+gpor[[3]] <- glmer(GPOR ~ mean_wetted_width + cobble + (1 | transect_num), family = poisson, data = sal_all)
+
+model_names = c("water", "pH + EC", "mean_wetted_width + cobble")
+aictab(cand.set = gpor, modnames = model_names)
+
+summary(gpor[[3]])
+plot(gpor[[3]])
+qqnorm(residuals(gpor[[2]]))
+boxplot(residuals(gpor[[2]]))
+hist(residuals(gpor[[2]]))
+# none are significant, residuals are okay
+
+# PRUB
+prub = list()
+
+prub[[1]] <- glmer(PRUB ~ water + (1 | transect_num), family = poisson, data = sal_all)
+
+prub[[2]] <- glmer(PRUB ~ pH + EC +  (1 | transect_num), family = poisson, data = sal_all)
+
+prub[[3]] <- glmer(PRUB ~ mean_wetted_width + cobble + (1 | transect_num), family = poisson, data = sal_all)
+
+model_names = c("water", "pH + EC", "mean_wetted_width + cobble")
+aictab(cand.set = prub, modnames = model_names)
+
+# response is constant since there was just one count
 
 
